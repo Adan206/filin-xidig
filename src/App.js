1a00481cd4d32cc1a77915c95234a8acd8d8eb86
Adan Abbi <adanweli@hotmail.com>
2022-01-30 22:19:54 -0800

    new changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+// import MovieDetail from "./components/MovieDetail/MovieDetail";
 import "./App.scss";
 
 function App() {
   return (
-    <div>
-      <h2>MOVIE APP</h2>
+    <div className='app'>
+      {/* <Router>
+        <Header></Header>
+        <div className='container'>
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/movie/:imdbID' component={MovieDetail} />
+            <Route component={PageNotFound} />
+          </Switch>
+        </div>
+        <Footer />
+      </Router> */}
+      <h4>This is the main app page</h4>
     </div>
   );
 }
